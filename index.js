5dced69e40b1c5e9b3bac268fd4b39fea49e91f8
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-05-12 15:48:35 -0300

    get request method

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string 
 const server = http.createServer((req, res) => {
+    // Get the URL and parse it 
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the HTTP Metheod
+    const method = req.method.toUpperCase();
+
+    // Send the response
     res.end('Hello world\n');
+
+    // Log the request path
+    console.log(`Request received on path: ${trimmedPath} with method: ${method}`)
 });
 
 // Start the server, and have it listen on port 3000
